00ecc499f2bdfe82e66846d68fde92db52f2d83b
ozkalai <melihozkalayy@gmail.com>
2021-07-06 00:45:10 +0300

    index page updated

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,22 @@
 import React, { useEffect } from "react";
-import { useQuery } from "react-query";
-import { getUsers } from "../src/services/get-users";
-import { UserItem } from "../src/components/user-item";
+import Navigation from "../src/components/navigation";
+import UserList from "../src/components/user-list";
+import Search from "../src/components/search";
 
 export default function Home() {
-  const { data, isFetching } = useQuery(
-    "users",
-    async () => {
-      return (await getUsers(1)).data;
-    },
-    {
-      refetchOnWindowFocus: false,
-      staleTime: Infinity,
-    }
-  );
-
-  if (isFetching) {
-    return <div>loading...</div>;
-  }
-
   return (
-    <div>
-      <ul>
-        {data?.data.map((user) => (
-          <UserItem user={user} />
-        ))}
-      </ul>
+    <div className="bg-main">
+      <Navigation />
+      <div className="w-full flex flex-col justify-center items-center sm:flex-row sm:justify-between px-4 2xl:px-24 xl:px-5 lg:px-5 md:px-16 mx-4 md:mt-10 md:mb-10 ">
+        <div
+          className=" font-bold text-xl   md:text-2xl lg:text-3xl w-1/2 flex my-4 md:my-0 justify-center items-center md:justify-start 
+         text-black"
+        >
+          USER LIST
+        </div>
+        <Search />
+      </div>
+      <UserList />
     </div>
   );
 }
