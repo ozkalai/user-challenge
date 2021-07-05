1e3d19c0124e5ced3a42f56563ebb12c046c925d
ozkalai <melihozkalayy@gmail.com>
2021-07-06 00:46:02 +0300

    Navigation comp updated

---
+++ src/components/navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div className="text-gray-600 bg-white w-full flex justify-center text-align-center items-center border-b-2 mx-auto text-lg md:text-lg lg:text-2xl xl:text-3xl font-bold md: p-4 hover:text-black">
+      Melih Özkalay
+    </div>
+  );
+}
+
+export default Navigation;
