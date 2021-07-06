7e523f5e0bc0ea3207a09653605bd5d3fc3cbfe8
ozkalai <melihozkalayy@gmail.com>
2021-07-06 20:25:43 +0300

    fixed some css

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,19 @@ import Search from "../src/components/search";
 
 export default function Home() {
   return (
-    <div className="bg-main">
+    <div className="bg-main min-h-screen">
       <Navigation />
-      <div className="w-full flex flex-col justify-center items-center sm:flex-row sm:justify-between px-4 2xl:px-24 xl:px-5 lg:px-5 md:px-16 mx-4 md:mt-10 md:mb-10 ">
+      <div className="w-11/12 flex flex-col my-10 items-center sm:flex-row sm:justify-between mx-auto sm:w-11/12 md:my-10 ">
         <div
-          className=" font-bold text-xl   md:text-2xl lg:text-3xl w-1/2 flex my-4 md:my-0 justify-center items-center md:justify-start 
-         text-black"
+          className=" font-bold text-xl md:text-2xl lg:text-3xl w-1/2 flex my-4 md:my-0 justify-center sm:justify-start items-center md:justify-start 
+         text-secondary"
         >
           USER LIST
         </div>
         <Search />
       </div>
       <UserList />
+      <div className="w-full h-20 bg-main"></div>
     </div>
   );
 }
