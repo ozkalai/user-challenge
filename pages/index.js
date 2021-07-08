11a82bff1b4728dbb9615e5b3cdb3fc7233b574d
ozkalai <melihozkalayy@gmail.com>
2021-07-09 02:53:04 +0300

    search filter completed

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "../src/components/navigation";
 import UserList from "../src/components/user-list";
-import Search from "../src/components/search";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Home() {
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleOnChange = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+  };
+
+  const results = filteredData.filter((user) => {
+    return user.firstName.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div className="bg-main min-h-screen">
       <Navigation />
@@ -14,10 +26,30 @@ export default function Home() {
         >
           USER LIST
         </div>
-        <Search />
+        <div className="w-full mx-0 mb-2 md:w-2/4 lg:w-1/4  ">
+          <div className="bg-white flex items-center shadow-xl">
+            <input
+              className="w-full px-6 text-gray-700 leading-tight focus:outline-none text-xs md:text-sm lg:text-lg"
+              id="search"
+              type="text"
+              placeholder="Search by name"
+              onChange={handleOnChange}
+              value={searchTerm}
+            />
+
+            <div className="p-4">
+              <button className="bg-secondary text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-8 h-8 flex items-center justify-center">
+                <AiOutlineSearch />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <UserList />
-      <div className="w-full h-20 bg-main"></div>
+      <UserList
+        filteredData={filteredData}
+        setFilteredData={setFilteredData}
+        results={results}
+      />
     </div>
   );
 }
