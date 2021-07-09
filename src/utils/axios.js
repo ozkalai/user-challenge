6088fd9e3eaffca21dc5c9c93dfeac1b18746363
ozkalai <melihozkalayy@gmail.com>
2021-07-09 14:58:27 +0300

    updated id

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const setAxiosHeaders = () => {
   axios.defaults.headers = {
-    "app-id": "60e80f3f05382821488dd1f4",
+    "app-id": "60e8394f2f3ecb24b3a9bc8e",
   };
   axios.defaults.baseURL = "https://dummyapi.io/data/api";
 };
