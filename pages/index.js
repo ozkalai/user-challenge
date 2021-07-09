c8e636c8a02bbf04a21d905b3e1b8d2a1c52c07c
ozkalai <melihozkalayy@gmail.com>
2021-07-09 14:58:19 +0300

    fixed search css

--- pages/index.js
+++ pages/index.js
@@ -21,22 +21,21 @@ export default function Home() {
         >
           USER LIST
         </div>
-        <div className="w-full mx-0 mb-2 md:w-2/4 lg:w-1/4  ">
+        <div className="w-full mx-0 mb-2 md:w-2/4 lg:w-1/4 flex bg-white ">
+          <div className="p-2 pr-0">
+            <div className="text-gray-700 text-white rounded-full p-2 focus:outline-none w-8 h-8 flex items-center justify-center">
+              <AiOutlineSearch />
+            </div>
+          </div>
           <div className="bg-white flex items-center shadow-xl">
             <input
-              className="w-full px-6 text-gray-700 leading-tight focus:outline-none text-xs md:text-sm lg:text-lg"
+              className="w-full px-2 text-gray-700 leading-tight focus:outline-none text-xs md:text-sm lg:text-lg"
               id="search"
               type="text"
               placeholder="Search by name"
               onChange={handleOnChange}
               value={searchTerm}
             />
-
-            <div className="p-4">
-              <button className="bg-secondary text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-8 h-8 flex items-center justify-center">
-                <AiOutlineSearch />
-              </button>
-            </div>
           </div>
         </div>
       </div>
